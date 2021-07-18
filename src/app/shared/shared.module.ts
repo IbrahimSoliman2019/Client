d8cd0b1e2eb53451e7d52fb5841ec17cc8c439b8
Ibrahimsoliman200 <engibrahimsoliman2019@gmail.com>
2021-07-18 11:13:40 +0200

    adding header and footer to shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagerComponent } from './Components/pager/pager.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [  ],
+  declarations: [  HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
