ef73d465511921867bff0cad978b84941a115156
Ibrahimsoliman200 <engibrahimsoliman2019@gmail.com>
2021-07-21 11:13:52 +0200

    error-handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import {ExploreModule} from './explore/explore.module';
 import {HomeModule} from './home/home.module';
 import {RegisterModule} from './register/register.module'
 import { RouterLink, RouterModule } from '@angular/router';
+import { ErrorInterceptorService } from './core/Interceptors/error-interceptor.service';
 //import {SharedModule} from './shared/shared/shared.module'
 
 
@@ -38,7 +39,10 @@ import { RouterLink, RouterModule } from '@angular/router';
 
   
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptorService,multi:true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NotFonundComponent } from './not-fonund/not-fonund.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [  NotFonundComponent, ServerErrorComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass:'toast-bottom-right',
+      preventDuplicates:true
+    })
   ],
   exports:[]
 })
