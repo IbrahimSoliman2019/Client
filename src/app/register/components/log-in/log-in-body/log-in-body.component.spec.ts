f952a20f3ae2a3f030834c787981fb3345ca7f69
Sherif Mohamed Mohamed El-Metwally <63125795+Sherif-Mohamed-Mohamed@users.noreply.github.com>
2021-07-22 23:04:33 +0200

    validacation register

--- src/app/register/components/log-in/log-in-body/log-in-body.component.spec.ts
+++ src/app/register/components/log-in/log-in-body/log-in-body.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LogInBodyComponent } from './log-in-body.component';
 
@@ -6,7 +6,7 @@ describe('LogInBodyComponent', () => {
   let component: LogInBodyComponent;
   let fixture: ComponentFixture<LogInBodyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LogInBodyComponent ]
     })
--- src/app/register/components/singup/singup.component.ts
+++ src/app/register/components/singup/singup.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../Model/user';
+import { UsersService } from '../../Services/users.service';
 
 @Component({
   selector: 'app-singup',
@@ -6,12 +10,99 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singup.component.scss']
 })
 export class SingupComponent implements OnInit {
+  fullName:string="";
+  email:string="";
+  password:string="";
+  conPassword:string="";
+  newUser:User;
+regex:RegExp;
+validatormessage:string;
+@ViewChild('signUp')signUpForm:NgForm;
 
-  constructor() { }
-  OnFormSave(log)
+
+
+  SignUp(sign:any)
   {
-    console.log("Submit form")
+    this.fullName=sign.value.fullName;
+    this.email=sign.value.email;
+    this.password=sign.value.password;
+    this.newUser=new User(this.fullName,this.email,this.password);
+
+
+    var check=this.UsSer.AddUser(this.newUser);
+
+    if (check==null) 
+    {
+      alert("this email is used");
+    }
+    else
+    {
+      alert("done");
+      this.route.navigateByUrl("/login");
+    }
+
+
   }
+
+  IsValidPass(){
+
+    const pass = this.signUpForm.value.password;
+  
+    if ( pass.length < 8) { 
+  
+      this.validatormessage= 'Password must contain at least 8 charater';
+  
+      return false;
+  
+    }
+  
+    this.regex = new RegExp('[a-z]');
+  
+    if (!this.regex.test(pass)) {
+  
+      this.validatormessage= 'Password must contain a small letter';
+  
+      return false;
+  
+    }
+  
+    this.regex = new RegExp('[A-Z]');
+  
+    if (!this.regex.test(pass)) {
+  
+      this.validatormessage= 'Password must contain a capital letter';
+  
+      return false;
+  
+    }
+  
+    this.regex = new RegExp('[~!@#$%^&*()<>]');
+  
+    if (!this.regex.test(pass)) {
+  
+      this.validatormessage= 'Password must contain a special character';
+  
+      return false;
+  
+    }
+  
+    this.regex = new RegExp('[0-9]');
+  
+    if (!this.regex.test(pass)) {
+  
+      this.validatormessage= 'Password must contain number';
+  
+      return false;
+  
+    }
+    this.validatormessage="";
+  return true ;
+  
+  
+  }
+
+  constructor(public UsSer:UsersService,public route:Router ) { }
+
   ngOnInit(): void {
   }
 
